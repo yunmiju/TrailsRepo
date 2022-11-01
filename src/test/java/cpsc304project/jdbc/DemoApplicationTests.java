42492d89a65874dba6c6bfe73903d054aa8828b7
Jieun Kim <jieunkxx@students.cs.ubc.ca>
2022-11-01 16:35:08 -0700

    Add: initialize project

---
+++ src/test/java/cpsc304project/jdbc/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package cpsc304project.jdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
